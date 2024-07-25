8e5d7ddb9e0333d7e6d77423b89a6a8af77b0dff
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-07-25 10:23:31 +0200

    17 - Tutor Website Project - Part 1 - Navbar Background On Scroll

--- js/script.js
+++ js/script.js
@@ -8,3 +8,13 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
+// Change navbar background on scroll
+window.addEventListener('scroll', function () {
+  const navbar = document.querySelector('.navbar');
+
+  if (window.scrollY > 0) {
+    navbar.classList.add('navbar-scroll');
+  } else {
+    navbar.classList.remove('navbar-scroll');
+  }
+});
